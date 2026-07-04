c3b564ddb13eab82ab255aa50dd22fde0e84e6af
Álex Filipe <hi@alexfili.pe>
2026-07-04 02:40:57 -0700

    Use explicit path redirects

--- www/_worker.js
+++ www/_worker.js
@@ -1,10 +1,33 @@
+const existingPaths = new Set([
+  "/",
+  "/assets/app-icon.png",
+  "/assets/apple-touch-icon.png",
+  "/assets/favicon-16.png",
+  "/assets/favicon-32.png",
+  "/assets/icon-192.png",
+  "/assets/icon-512.png",
+  "/assets/mark-black.png",
+  "/assets/mark-gradient.png",
+  "/assets/mark-white.png",
+  "/assets/social-preview.png",
+  "/assets/ui-activity.png",
+  "/assets/ui-conflicts.png",
+  "/assets/ui-overview-scanning.png",
+  "/assets/ui-overview.png",
+  "/assets/ui-settings.png",
+  "/assets/ui-sync-sets.png",
+  "/index.html",
+  "/robots.txt",
+  "/site.webmanifest",
+  "/sitemap.xml",
+]);
+
 export default {
   async fetch(request, env) {
     const url = new URL(request.url);
-    const response = await env.ASSETS.fetch(request);
 
-    if (response.status !== 404 || url.pathname === "/") {
-      return response;
+    if (existingPaths.has(url.pathname)) {
+      return env.ASSETS.fetch(request);
     }
 
     const home = new URL(request.url);
